aa8a915e6c661e79ddaf679f6f2a6415d3bb835b
Ali Hussnain <ali.hussnain112003@gmail.com>
2023-03-11 15:36:51 +0500

    Project detail page added

---
+++ project-detail.php
@@ -0,0 +1,143 @@
+<?php include("./includes/header.php") ?>
+<main>
+    <section class="mt-80 poition-relative project-detail-profile-section">
+        <div>
+            <img src="/assets/images/project_banner_image.png" class="banner-img" height="320" alt="">
+        </div>
+        <div class="theme-layout">
+            <div class=" nft-profile ms-xl-110">
+                <img src="/assets/images/nft_project.png" class="rounded-circle profile-img" width="260" height="260" alt="">
+                <div class="details">
+                    <h5 class="theme-text-lg theme-fw-400">Pre sale: 29 Jan</h5>
+                    <div class="d-flex align-items-center gap-3 flex-wrap">
+                        <h3 class="col-12 col-md-auto theme-h3 theme-fw-700">Cat Fight P2E NFT</h3>
+                        <div class="col-auto d-inline-flex gap-2">
+                            <img src="/assets/images/icons/verified_badge.svg" height="30" alt="">
+                            <img src="/assets/images/icons/doxxed_kyc_badge.png" height="30" alt="">
+                        </div>
+                    </div>
+                    <div class="d-flex mt-4">
+                        <div class="bg-light px-3 py-2 rounded-pill text-dark d-flex align-items-center gap-2">
+                            <img src="/assets/images/icons/price_icon_dark.svg" width="12" height="20" alt="">
+                            <span class="theme-text-body theme-fw-400 text-uppercase">Ethereum</span>
+                        </div>
+                        <div class="vertical-divider mx-20"></div>
+                        <div class="bg-light-10 px-3 py-2 rounded-pill text-light d-flex align-items-center gap-2">
+                            <span class="theme-text-body theme-fw-400">9999 Total Supply</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bg-gradient-secondary">
+            <div class="theme-layout py-75 my-60">
+                <div class="ms-xl-110 row row-cols-1 row-cols-md-2 row-cols-lg-4 row-gap-40 g-0">
+                    <div class="col text-center">
+                        <p class="theme-text-body text-light theme-fw-400">Pre-sale Date:</p>
+                        <p class="theme-text-body text-light theme-fw-600">29 Jan 2023-05:00 am (UTC)</p>
+                    </div>
+                    <div class="col text-center">
+                        <p class="theme-text-body text-light theme-fw-400">Pre-sale Price:</p>
+                        <p class="theme-text-body text-light theme-fw-600">0.05 ETH</p>
+                    </div>
+                    <div class="col text-center">
+                        <p class="theme-text-body text-light theme-fw-400">Public Mint Date:</p>
+                        <p class="theme-text-body text-light theme-fw-600">31 Jan 2023-05:00 am (UTC)</p>
+                    </div>
+                    <div class="col text-center">
+                        <p class="theme-text-body text-light theme-fw-400">Mint Price:</p>
+                        <p class="theme-text-body text-light theme-fw-600">0.15 ETH</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="theme-layout">
+            <div class="mx-xl-110 row justify-content-between mb-140 g-0">
+                <div class="col-lg-8" style="max-width: 700px;">
+                    <div class="pb-3 border-bottom-muted">
+                        <p class="theme-text-body theme-fw-600 text-primary">Overview</p>
+                    </div>
+                    <div class="mt-20">
+                        <div class="row">
+                            <p class="col-3 theme-text-body theme-fw-400 text-light-70" style="max-width: 140px; min-width: 90px;">Description</p>
+                            <p class="col theme-text-body theme-fw-400 text-light">
+                                Welcome to the Cat Fighters family!
+                                <br><br>
+                                Cat Fight NFT is a 3D fighting and surviving game built with Unreal Engine 5 on the Solana blockchain. You’ll be able to play, gain experience and earn $$$ using the game’s crypto token $CATF.
+                                <br>
+                                MINT DATE: 28-01-2023
+                                <br>
+                                PFP SUPPLY: 9,999
+                                <br>
+                                LAND SUPPLY: 55k
+                                <br>
+                                BLOCKCHAIN: SOL
+                                <br><br>
+                                For every 2 PFP NFTs you hold, we will airdrop 1 Plot of Land for FREE.
+                                <br><br>
+                                ***DETAILS AND BENEFITS***
+                                <br>
+                                * NFT staking and P2E games..
+                                <br>
+                                * Holders of CATFIGHT NFT will have access to the $CATF token, which can be used to reward players in-game, as well as mint future collections or swap for USDT to be able to use in the real world.
+                                <br>
+                                * Players and holders can purchase various clothes and accessories for their avatars..
+                                <br>
+                                * Citizens of Cat Fight island can purchase land, which opens up additional opportunities
+                                <br>
+                                * WL spots are still open… 1,000 spots available
+                            </p>
+                        </div>
+                        <div class="row mt-80 align-items-center">
+                            <p class="col-3 theme-text-body theme-fw-400 text-light-70" style="max-width: 145px; min-width: 90px;">Social Profiles:</p>
+                            <div class="col row g-0">
+                                <p class="col-auto theme-text-sm theme-fw-400 text-light">
+                                    <i class="text-primary fa-brands fa-twitter"></i>
+                                    <span class="ms-2">Twitter (2.4K)</span>
+                                </p>
+                                <p class="col-auto ms-4 theme-text-sm theme-fw-400 text-light">
+                                    <i class="text-primary fa-brands fa-discord"></i>
+                                    <span class="ms-2">Discord (3.2K)</span>
+                                </p>
+                                <p class="col-auto ms-4 theme-text-sm theme-fw-400 text-light">
+                                    <i class="text-primary fa-solid fa-globe"></i>
+                                    <span class="ms-2">Website</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row mt-30 align-items-center">
+                            <p class="col-3 theme-text-body theme-fw-400 text-light-70" style="max-width: 145px; min-width: 90px;">Event Tags:</p>
+                            <div class="col row g-0">
+                                <p class="col-auto theme-text-sm theme-fw-400 text-light">#FreeMint</p>
+                                <p class="col-auto ms-4 theme-text-sm theme-fw-400 text-light">Metaverse</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-none d-lg-block ps-3 ps-xxl-0 col-lg-4" style="max-width: 300px;">
+                    <div class="pb-3 border-bottom-muted">
+                        <p class="theme-text-body theme-fw-600 text-primary">Register for this event</p>
+                    </div>
+                    <div class="mt-20">
+                        <div class="w-100 mb-20">
+                            <label class="form-label fs-14 text-light-70 mb-10">Wallet address</label>
+                            <input type="text" class="form-control fs-14 shadow-none" placeholder="" />
+                        </div>
+                        <div class="w-100 mb-20">
+                            <label class="form-label fs-14 text-light-70 mb-10">Twitter username</label>
+                            <input type="text" class="form-control fs-14 shadow-none" placeholder="" />
+                        </div>
+                        <div class="w-100 mb-20">
+                            <label class="form-label fs-14 text-light-70 mb-10">Email</label>
+                            <input type="text" class="form-control fs-14 shadow-none" placeholder="" />
+                        </div>
+                        <div class="d-flex">
+                            <a href="/success-page.php" class="btn-theme px-5 h-46">Submit</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<?php include("./includes/footer.php") ?>
