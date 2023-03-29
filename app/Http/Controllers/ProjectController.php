b1156063428c455ee6e1579a9a119402959ba58d
Hamza Saqib <mianhamza7262@gmail.com>
2023-03-29 19:07:33 +0500

    show page project featured issue fix

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -147,20 +147,21 @@ class ProjectController extends Controller
      */
     public function show(Request $request, Project $project)
     {
+        $featuredProjects = Project::where('is_featured', true)->where('status', 'published')->orderBy('id', 'ASC')->get()->take(6);
+        $mintingSoonProjects = Project::where('status', 'published')->where('mint_time', '>=', Carbon::now())
+            ->where('mint_time', '<=', Carbon::now()->addWeek(1))
+            ->orderBy('id', 'ASC')->get()->take(6);
+
         $cookie_name = (Str::replace('.', '', ($request->ip())) . '-' . $project->id);
         if (Cookie::get($cookie_name) == '') {
             $cookie = cookie($cookie_name, '1', 60); //set the cookie
             $currentViews = $project->page_views;
             $project->page_views = $currentViews + 1;
             $project->save();
-            return response()->view('pages.projects.show', compact('project'))
+            return response()->view('pages.projects.show', compact('project', 'featuredProjects', 'mintingSoonProjects'))
                 ->withCookie($cookie);
         }
 
-        $featuredProjects = Project::where('is_featured', true)->where('status', 'published')->orderBy('id', 'ASC')->get()->take(6);
-        $mintingSoonProjects = Project::where('status', 'published')->where('mint_time', '>=', Carbon::now())
-            ->where('mint_time', '<=', Carbon::now()->addWeek(1))
-            ->orderBy('id', 'ASC')->get()->take(6);
 
         return view('pages.projects.show', compact('project', 'featuredProjects', 'mintingSoonProjects'));
     }
